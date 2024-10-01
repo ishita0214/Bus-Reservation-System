93ed187dee29fa64e36d89fa974534db46171969
Namrata <namratapanjwani1412@gmail.com>
2024-10-01 09:38:36 +0530

    home page added

--- Frontend/bus-reservation-frontend/src/app/home-page/home-page.component.ts
+++ Frontend/bus-reservation-frontend/src/app/home-page/home-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CalendarModule],
+  imports: [MatDatepickerModule,CalendarModule,FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
   date1: Date | undefined;
-
 }
